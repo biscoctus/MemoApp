130568c6334367ee968095ebd7cddae853f974e7
biscoctus <7844404+biscoctus@users.noreply.github.com>
2022-01-27 14:00:04 +0900

    Install @expo/vector-icons

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { string } from 'prop-types';
+import { View, StyleSheet } from 'react-native';
+import { string, shape } from 'prop-types';
+import { Feather } from '@expo/vector-icons';
 
 export default function CircleButton(props) {
-  const { children } = props;
+  const { style, name } = props;
   return (
-    <View style={styles.circleButton}>
-      <Text style={styles.circleButtonLabel}>{ children }</Text>
+    <View style={[styles.circleButton, style]}>
+      <Feather name={name} size={32} color="white" />
     </View>
   );
 }
 
 CircleButton.propTypes = {
-  children: string.isRequired,
+  style: shape(),
+  name: string.isRequired,
+};
+
+CircleButton.defaultProps = {
+  style: null,
 };
 
 const styles = StyleSheet.create({
@@ -27,7 +33,7 @@ const styles = StyleSheet.create({
     right: 40,
     bottom: 40,
     shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8},
+    shadowOffset: { width: 0, height: 8 },
     shadowOpacity: 0.25,
     shadowRadius: 8,
     elevation: 8,
